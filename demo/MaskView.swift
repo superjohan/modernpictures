8e7fb4de1e71862c2d108d12ed0f203e17fc69b0
Johan Halin <johan@halin.me>
2019-02-14 19:57:52 +0200

    Mask view. Only let certain segments of the view show.

---
+++ demo/MaskView.swift
@@ -0,0 +1,40 @@
+//
+//  MaskedView.swift
+//  demo
+//
+//  Created by Johan Halin on 14/02/2019.
+//  Copyright © 2019 Dekadence. All rights reserved.
+//
+
+import UIKit
+
+class MaskView: UIView {
+    private let startOffset: CGFloat
+    
+    init(frame: CGRect, offset: CGFloat) {
+        self.startOffset = offset
+        
+        super.init(frame: frame)
+        
+        self.backgroundColor = .clear
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+
+        var position = self.startOffset
+
+        while position < rect.width {
+            context.setStrokeColor(UIColor.white.cgColor)
+            context.setLineWidth(1.0 / self.contentScaleFactor)
+            context.move(to: CGPoint(x: position, y: 0))
+            context.addLine(to: CGPoint(x: position, y: rect.height))
+            context.strokePath()
+            position += 4
+        }
+    }
+}
