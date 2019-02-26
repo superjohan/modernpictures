3e1f6c1ebf6e03f0e3bf5612eb7c4ee35c07e2ef
Johan Halin <johan@halin.me>
2019-02-26 19:57:09 +0200

    Small vocabulary of words. Can be expanded later.

---
+++ demo/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  demo
+//
+//  Created by Johan Halin on 26/02/2019.
+//  Copyright © 2019 Dekadence. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let vocabulary = [
+        "modern", "pictures", "members", "super", "hyper", "green", "precarity", "austerity", "violence", "brutal", "horizon", "chain", "street", "man", "woman", "boy", "girl", "simple", "hunters", "perform", "persist", "resist", "compute", "create", "grotesk", "devotion", "quick"
+    ]
+}
